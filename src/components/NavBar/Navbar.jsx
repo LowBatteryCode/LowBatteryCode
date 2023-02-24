05ef887f11394601a9735f69df66932f330175bd
Sebastian <lowbatterycode@gmail.com>
2023-02-24 17:02:45 -0500

    Primera version de la Navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,24 +2,22 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="grid grid-cols-4 bg-lime-500 ">
-      <h1 className="col-span-2 text-3xl">Low Battery Code</h1>
-      <nav className="col-span-2">
-        <ul className="">
+    <div className="grid grid-cols-4 bg-green-500">
+      <h1 className="col-span-2 text-4xl mb-2 ml-2 font-bold text-white">Low Battery Code</h1>
+        <ul className="col-span-2 flex justify-end my-auto">
           <li>
-            <NavLink to="/" className='mr-5'>Home</NavLink>
+            <NavLink to="/" className='mr-5 text-white font-bold hover:text-cyan-200'>Home</NavLink>
           </li>
           <li>
-            <NavLink to="/Blog" className='mr-5'>Blog</NavLink>
+            <NavLink to="/Blog" className='mr-5 text-white font-bold hover:text-cyan-200'>Blog</NavLink>
           </li>
           <li>
-            <NavLink to="/Actualizaciones" className='mr-5'>Actualizaciones</NavLink>
+            <NavLink to="/Actualizaciones" className='mr-5 text-white font-bold hover:text-cyan-200'>Actualizaciones</NavLink>
           </li>
           <li>
-            <NavLink to="/Contacto" className='mr-5'>Contacto</NavLink>
+            <NavLink to="/Contacto" className='mr-5 text-white font-bold hover:text-cyan-200'>Contacto</NavLink>
           </li>
         </ul>
-      </nav>
     </div>
   );
 }
